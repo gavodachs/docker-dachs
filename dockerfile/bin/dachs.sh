d8f1fd3006b2990fb866c66af3503ca30feace65
Markus Demleitner <msdemlei@ari.uni-heidelberg.de>
2016-11-18 15:45:38 +0100

    First steps towards getting rid of patching rc (plus some other beautifications)

--- dockerfile/bin/dachs.sh
+++ dockerfile/bin/dachs.sh
@@ -13,4 +13,9 @@ echo 'pointing to this container here.'
 echo '==================================================='
 echo ''
 
-service postgresql start 
+export PGDATA=/var/lib/postgresql/9.4 
+# postgres needs this directory for log files; then start postgres
+mkdir -p -m 777 /var/run/postgresql/9.4-main.pg_stat_tmp/
+su - postgres  -c "/usr/lib/postgresql/9.4/bin/postgres \
+	-c config_file=/etc/postgresql/9.4/main/postgresql.conf \
+	-c logging_collector=on"

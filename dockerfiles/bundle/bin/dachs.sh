63f0fd488b7d8eb47825b8ba52de8a544e792ecd
Carlos H Brandt <carloshenriquebrandt@gmail.com>
2020-05-27 01:35:00 +0200

    Add dachs start script

---
+++ dockerfiles/bundle/bin/dachs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+
+#ENV="env -i LANG=C PATH=/usr/local/bin:/usr/bin:/bin"
+. /lib/lsb/init-functions
+test -f /etc/default/rcS && . /etc/default/rcS
+test -f /etc/default/apache2 && . /etc/default/apache2
+#SERVER_BIN="$ENV /usr/bin/dachs --disable-spew serve"
+
+SERVER_BIN="/usr/bin/dachs --disable-spew serve"
+
+case $1 in
+	debug)
+    $SERVER_BIN debug && log_end_msg 0 || log_end_msg 1
+	;;
+	start)
+		if $SERVER_BIN start; then
+			log_end_msg 0
+		else
+			log_end_msg 1
+		fi
+	;;
+	stop)
+		log_daemon_msg "Stopping VO server" "dachs"
+		if $SERVER_BIN stop; then
+			log_end_msg 0
+		else
+			log_end_msg 1
+		fi
+	;;
+	reload | force-reload)
+		log_daemon_msg "Reloading VO server config" "dachs"
+		if $SERVER_BIN reload $2 ; then
+			log_end_msg 0
+		else
+			log_end_msg 1
+		fi
+	;;
+	restart)
+		log_daemon_msg "Restarting VO server" "dachs"
+		if $SERVER_BIN restart; then
+			log_end_msg 0
+		else
+			log_end_msg 1
+		fi
+	;;
+	*)
+		log_success_msg "Usage: /etc/init.d/dachs {start|stop|debug|restart|reload|force-reload}"
+		exit 1
+	;;
+esac

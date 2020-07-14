eedd7a00dec33f808aac96cdf636b0ae06937860
Carlos H Brandt <carloshenriquebrandt@gmail.com>
2020-07-14 13:38:28 +0200

    [bla] fix whitespaces

--- dockerfiles/bundle/bin/dachs.sh
+++ dockerfiles/bundle/bin/dachs.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -e
 
 dachs_start() {
-	SERVE="$1"
+  SERVE="$1"
 }
 
 case $1 in
-	debug)
-		service postgresql status || service postgresql start
-		service dachs start --debug
-	;;
-	start)
-		service postgresql status || service postgresql start
-		service dachs start
-	;;
-	stop)
-		service dachs stop
-	;;
-	*)
-		THIS=$(basename $BASH_SOURCE)
-		echo ""
-		echo "Usage: $THIS {start|stop|debug}"
-		echo ""
-		exit 1
-	;;
+  debug)
+    service postgresql status || service postgresql start
+    service dachs start --debug
+  ;;
+  start)
+    service postgresql status || service postgresql start
+    service dachs start
+  ;;
+  stop)
+    service dachs stop
+  ;;
+  *)
+    THIS=$(basename $BASH_SOURCE)
+    echo ""
+    echo "Usage: $THIS {start|stop|debug}"
+    echo ""
+    exit 1
+  ;;
 esac
